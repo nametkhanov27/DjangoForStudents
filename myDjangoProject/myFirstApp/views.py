9c1977039a11f47d7b611f12af4b4990d85bb797
shynggys9219 <alshynggys@mail.ru>
2019-10-07 15:55:21 +0600

    Lecture 6 Monday commit

--- myDjangoProject/myFirstApp/views.py
+++ myDjangoProject/myFirstApp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+# '   GET/HEAD/POST nameOfFile 1.0/1.1
     return HttpResponse(
     "<center><b><h2>I hope today they will give my salary. \
     Otherwise I will not have any motivation :( <h2></b></center>"

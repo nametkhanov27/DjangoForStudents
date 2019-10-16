e43ee9ad56c17cffb40b442c51f79a3820d387ba
shynggys9219 <alshynggys@mail.ru>
2019-10-16 12:45:20 +0600

    Lecture 7 data was added

--- myDjangoProject/myFirstApp/models.py
+++ myDjangoProject/myFirstApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+	article_name = models.CharField(max_length = 1000)
+	article_text = models.TextField()
+	article_date = models.DateTimeField('date published')
+	
+	def __str__(self):
+		return self.article_name
+		
+class Editor(models.Model):
+	editor_name = models.CharField(max_length = 100)
+	editor_surname = models.CharField(max_length = 100)
+	
+	def __str__(self):
+		return self.editor_name + " " + self.editor_surname 
+		
+class User(models.Model):
+	user_name = models.CharField(max_length = 100)
+	user_surname = models.CharField(max_length = 100)
+	user_dob = models.DateTimeField('date of birth')
+	
+	def __str__(self):
+		return self.user_name + " " + self.user_surname
